4c5f350fdde2aa667e7622d205e2c78862ec97bc
quofiansah <emmanuelansah247@gmail.com>
2018-02-28 23:47:05 +0000

    new files added

--- Node/routes/tasks.js
+++ Node/routes/tasks.js
@@ -7,7 +7,13 @@ var path = require('path');
 var nodemailer = require('nodemailer');
 var MailData = require('../Models/maildata');
 var SchoolInfo = require('../Models/schoolinfo');
+var client = require('twilio')('ACf81f3c57ad78771cbd57c84b04761c7f','c2f2275a9e5006183de32a965b44acf4');
 
+
+router.get('/test',function(req,res,next){
+    
+    res.send("Application Is Currently Running");
+})
 
 router.post('/writeError',function(req,res,next){
    
@@ -41,12 +47,27 @@ router.get('/readData',function(req,res,next){
     res.json(ReadData("data"));
 })
 
+router.get('/sendSMS',function(req,res,next){
+    
+    res.json(SendSMS());
+})
+
 router.post('/sendMail',function(req,res,next){
-    var schooldata = new SchoolInfo("","","");
+    var schooldata = new SchoolInfo("","","","","","");
+    var n=0;
     for(var key in schooldata){
-        schooldata[key] = ReadData(key);
+        schooldata[n] = ReadData(key);
+        n++;
     }
-    var maildata = new MailData(req.body.params.email,req.body.params.subject,req.body.params.message);
+    if(!req.body){
+        status= 400;
+        res.json({
+            "error": "Bad Data"
+        })
+    }
+    
+
+    var maildata = new MailData(req.body.email,req.body.subject,req.body.message);
     if(!maildata){
         status= 400;
         res.json({
@@ -54,18 +75,45 @@ router.post('/sendMail',function(req,res,next){
         })
        }
         else{
+            console.log("succes");            
         res.json(SendMail(maildata,schooldata));
         }
 })
 
-function SendMail(maildata,schooldata){        
+
+router.post('/sendMailToSelf',function(req,res,next){
+    if(!req.body){
+        status= 400;
+        res.json({
+            "error": "Bad Data"
+        })
+    }
+    
+
+    var maildata = new MailData(req.body.emaill,req.body.subject,req.body.message);
+    if(!maildata){
+        status= 400;
+        res.json({
+            "error": "Bad Data"
+        })
+       }
+        else{          
+        res.json(SendMailToSelf(maildata,req.body.selfemail));
+        }
+})
+
+
+
+function SendMail(maildata,schooldata){  
+    
+    
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         secure:false,
         port:25,
         auth: {
-            user: schooldata.email,
-            pass: schooldata.password
+            user: schooldata[1],
+            pass: schooldata[2]
         },
         tls:{
             rejectUnauthorized:false
@@ -73,7 +121,7 @@ function SendMail(maildata,schooldata){
     })
 
     let HelperOptions = {
-        from: schooldata.name + '<' + schooldata.email + '>',
+        from: schooldata[0] + '<' + schooldata[1] + '>',
         to: maildata.email,
         subject: maildata.subject,
         text: maildata.message
@@ -88,6 +136,53 @@ function SendMail(maildata,schooldata){
             return(info);
         }
     });
+}
+
+
+function SendMailToSelf(maildata,selfemail){  
+    
+    
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure:false,
+        port:25,
+        auth: {
+            user: 'emmanuelansah247@gmail.com',
+            pass: 'P@$#$w854erd_2'
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+    })
+
+    let HelperOptions = {
+        from: maildata.email + '<' + maildata.email + '>',
+        to: selfemail,
+        subject: maildata.subject,
+        text: maildata.message
+    };
+
+    transporter.sendMail(HelperOptions,(error,info)=>{
+        if(error){
+            LogError(error);
+            return("error sending mail");
+        }
+        else{
+            return(info);
+        }
+    });
+}
+
+
+function SendSMS(){
+    console.log("some");
+    client.messages.create({
+        to:'+233571683961',
+        from:'+19386666612',
+        body: 'hello Quofi Ansah'
+    },(err,data)=>{
+        return(err + data);
+    })
 }
 
 
@@ -114,9 +209,10 @@ function WriteData(task){
 }
 
 function ReadData(filename){
-    var fileDirectorypath = './WrittenData/' + filename + '.txt';
-    if(!fs.existsSync(fileDirectorypath)){
-        shell.mkdir("-p",fileDirectorypath);
+    var fileDirectorypath = './WrittenData/' + 'schoolbaseFolder/' + filename + '.txt';
+    var fileDirectorybase = './WrittenData/' + "schoolbaseFolder/";
+    if(!fs.existsSync(fileDirectorybase)){
+        shell.mkdir("-p",fileDirectorybase);
         return("");
     }
     else{
